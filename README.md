9019a5ae85016428ed5247795fedb1d7042b9846
Николай Челюканов <nikochelyukanov@gmail.com>
2023-07-31 15:22:15 +0300

    Workflow_v1

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # https://nikochelyukanov.ddns.net/
 ### admin login: admin
-### admin passsword: niko12345
+### admin password: niko12345
 
--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -10,7 +10,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY', 'secret_key')
 
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['130.193.54.8', '127.0.0.1', 'localhost', 'nikochelyukanov.ddns.net']
 
